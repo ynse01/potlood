7257b5e170afe6ee5af1dc0ae77e2b51745fcbab
ynse <laptop@ynse.nl>
2019-06-02 19:20:57 +0200

    Remove font selection UI

--- demo/demo.js
+++ demo/demo.js
@@ -1,5 +1,4 @@
 import { Yord } from '../build/module/yord.js';
-import { Fonts } from '../build/module/fonts.js';
 
 "use strict";
 
@@ -8,24 +7,5 @@ var yord;
 export function init() {
     var content = document.getElementById("content");
     yord = new Yord(content);
-    fillFontFamilyList();
     yord.loadDocxFromUrl('./demo.docx');
-}
-
-export function onFontChanged() {
-    var fontFamilyElement = document.getElementById("font-family");
-    var fontFamily = fontFamilyElement.options[fontFamilyElement.selectedIndex].value;
-    var fontSize = document.getElementById("font-size").value;
-    // TODO: Re-implement yord.updateFont(fontFamily, fontSize);
-}
-
-function fillFontFamilyList() {
-    var families = Fonts.availableFonts();
-    var select = document.getElementById("font-family");
-    families.forEach((family) => {
-        var option = document.createElement("option");
-        option.text = family;
-        option.value = family;
-        select.add(option);
-    });
 }
