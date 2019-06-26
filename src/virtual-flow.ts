de8e4ebc3dddf0dda8dc63ab0947026fca41c14b
ynse <laptop@ynse.nl>
2019-06-26 12:55:42 +0200

    Read page width from section

--- src/virtual-flow.ts
+++ src/virtual-flow.ts
@@ -1,11 +1,26 @@
 import { WordDocument } from "./word-document.js";
 import { FlowPosition } from "./flow-position.js";
+import { Metrics } from "./metrics.js";
 
 export class VirtualFlow {
-    private _width: number;
+    // private _width: number;
+    private _pageWidth: number;
+    // private _pageHeight: number;
 
-    constructor(content: Element, _doc: WordDocument) {
-        this._width = content.clientWidth - 2 * 40;
+    constructor(_content: Element, doc: WordDocument) {
+        // this._width = content.clientWidth - 2 * 40;
+        this._pageWidth = 700;
+        // this._pageHeight = 400;
+        if (doc.section !== undefined) {
+            const pageWidth = doc.section.pageWidth;
+            if (pageWidth !== undefined) {
+                this._pageWidth = Metrics.convertPointToPixels(pageWidth) * 2.2;
+            }
+            const pageHeight = doc.section.pageHeight;
+            if (pageHeight !== undefined) {
+                // this._pageHeight = Metrics.convertPointsToPixels(pageHeight) * 2.2;
+            }
+        };
     }
 
     public getX(_flowPos: FlowPosition): number {
@@ -17,6 +32,6 @@ export class VirtualFlow {
     }
 
     public getWidth(_flowPos: FlowPosition): number {
-        return this._width;
+        return this._pageWidth;
     }
 }
