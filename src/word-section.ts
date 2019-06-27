62e78059d50ae0fdd61d7f3e43d5f19c51f62782
ynse <laptop@ynse.nl>
2019-06-27 10:19:47 +0200

    Make reading section attributes lazy

--- src/word-section.ts
+++ src/word-section.ts
@@ -4,112 +4,80 @@ import { Metrics } from "./metrics.js";
 
 export class WordSection {
     private sectionNode: ChildNode;
-    
+    private _pageHeight: number | undefined;
+    private _pageWidth: number | undefined;
+    private _marginTop: number | undefined;
+    private _marginLeft: number | undefined;
+    private _marginBottom: number | undefined;
+    private _marginRight: number | undefined;
+    // TODO: SectionType, PageOrientation and PageNumberFormat
+
     constructor(_doc: WordDocument, sectionNode: ChildNode) {
         this.sectionNode = sectionNode;
     }
 
     public get pageHeight(): number | undefined {
-        let pageHeight: number | undefined = undefined;
-        const pageSize = Xml.getFirstChildOfName(this.sectionNode, "w:pgSz") as Element;
-        if (pageSize !== undefined) {
-            const height = pageSize.getAttribute("w:h");
-            if (height !== null) {
-                pageHeight = Metrics.convertTwipsToPixels(parseInt(height, 10));
-            }
-        }
-        return pageHeight;
+        this.parseContent();
+        return this._pageHeight;
     }
 
     public get pageWidth(): number | undefined {
-        let pageWidth: number | undefined = undefined;
-        const pageSize = Xml.getFirstChildOfName(this.sectionNode, "w:pgSz") as Element;
-        if (pageSize !== undefined) {
-            const width = pageSize.getAttribute("w:w");
-            if (width !== null) {
-                pageWidth = Metrics.convertTwipsToPixels(parseInt(width, 10));
-            }
-        }
-        return pageWidth;
-    }
-
-    public get pageOrientation(): string {
-        let pageOrientation = "portrait";
-        const pageSize = Xml.getFirstChildOfName(this.sectionNode, "w:pgSz") as Element;
-        if (pageSize !== undefined) {
-            const orientation = pageSize.getAttribute("w:orient");
-            if (orientation !== null) {
-                pageOrientation = orientation;
-            }
-        }
-        return pageOrientation;
-    }
-
-    public get sectionType(): string {
-        let sectionType = Xml.getStringValueFromNode(this.sectionNode, "w:type");
-        if (sectionType === undefined) {
-            sectionType = "nextPage";
-        }
-        return sectionType;
-    }
-
-    public get pageNumberFormat(): string {
-        let pageNumberFormat = "decimal";
-        const pageSize = Xml.getFirstChildOfName(this.sectionNode, "w:pgNumType") as Element;
-        if (pageSize !== undefined) {
-            const format = pageSize.getAttribute("w:fmt");
-            if (format !== null) {
-                pageNumberFormat = format;
-            }
-        }
-        return pageNumberFormat;
+        this.parseContent();
+        return this._pageWidth;
     }
 
     public get marginTop(): number | undefined {
-        let marginTop: number | undefined = undefined;
-        const margin = Xml.getFirstChildOfName(this.sectionNode, "w:pgMar") as Element;
-        if (margin !== undefined) {
-            const top = margin.getAttribute("w:top");
-            if (top !== null) {
-                marginTop = parseInt(top);
-            }
-        }
-        return marginTop;
+        this.parseContent();
+        return this._marginTop;
     }
 
     public get marginLeft(): number | undefined {
-        let marginLeft: number | undefined = undefined;
-        const margin = Xml.getFirstChildOfName(this.sectionNode, "w:pgMar") as Element;
-        if (margin !== undefined) {
-            const left = margin.getAttribute("w:left");
-            if (left !== null) {
-                marginLeft = parseInt(left);
-            }
-        }
-        return marginLeft;
+        this.parseContent();
+        return this._marginLeft;
     }
 
     public get marginBottom(): number | undefined {
-        let marginBottom: number | undefined = undefined;
-        const margin = Xml.getFirstChildOfName(this.sectionNode, "w:pgMar") as Element;
-        if (margin !== undefined) {
-            const bottom = margin.getAttribute("w:bottom");
-            if (bottom !== null) {
-                marginBottom = parseInt(bottom);
-            }
-        }
-        return marginBottom;
+        this.parseContent();
+        return this._marginBottom;
     }
 
     public get marginRight(): number | undefined {
-        let marginRight: number | undefined = undefined;
-        const margin = Xml.getFirstChildOfName(this.sectionNode, "w:pgMar") as Element;
-        if (margin !== undefined) {
-            const right = margin.getAttribute("w:right");
-            if (right !== null) {
-                marginRight = parseInt(right);
+        this.parseContent();
+        return this._marginRight;
+    }
+
+    private parseContent(): void {
+        if (this._pageWidth === undefined) {
+            const pageSize = Xml.getFirstChildOfName(this.sectionNode, "w:pgSz") as Element;
+            if (pageSize !== undefined) {
+                const width = pageSize.getAttribute("w:w");
+                if (width !== null) {
+                    this._pageWidth = Metrics.convertTwipsToPixels(parseInt(width, 10));
+                }
+                const height = pageSize.getAttribute("w:h");
+                if (height !== null) {
+                    this._pageHeight = Metrics.convertTwipsToPixels(parseInt(height, 10));
+                }
+            }
+            const margin = Xml.getFirstChildOfName(this.sectionNode, "w:pgMar") as Element;
+            if (margin !== undefined) {
+                const top = margin.getAttribute("w:top");
+                if (top !== null) {
+                    this._marginTop = parseInt(top);
+                }
+                const left = margin.getAttribute("w:left");
+                if (left !== null) {
+                    this._marginLeft = parseInt(left);
+                }
+                const bottom = margin.getAttribute("w:bottom");
+                if (bottom !== null) {
+                    this._marginBottom = parseInt(bottom);
+                }
+                const right = margin.getAttribute("w:right");
+                if (right !== null) {
+                    this._marginRight = parseInt(right);
+                }
             }
         }
-        return marginRight;
     }
 }
