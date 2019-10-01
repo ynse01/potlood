6364f7371cc3a3ba974ef9fd532405d7cea8d41a
ynse <laptop@ynse.nl>
2019-10-01 20:03:59 +0200

    Code improvements

--- src/text/text-renderer.ts
+++ src/text/text-renderer.ts
@@ -2,7 +2,7 @@ import { VirtualFlow } from "../utils/virtual-flow.js";
 import { TextRun } from "./text-run.js";
 import { IPositionedTextLine } from "./positioned-text-line.js";
 import { UnderlineMode } from "./run-style.js";
-import { IPainter, IRectangle } from "../painting/i-painter.js";
+import { IPainter } from "../painting/i-painter.js";
 import { Style } from "./style.js";
 
 export class TextRenderer {
@@ -14,29 +14,25 @@ export class TextRenderer {
 
     public renderTextRun(run: TextRun, flow: VirtualFlow): void {
         run.getFlowLines(flow).forEach((line: IPositionedTextLine) => {
-            this.renderText(line, run.style);
+            this._renderText(line, run.style);
         });
     }
     
-    private renderText(line: IPositionedTextLine, style: Style): void {
-        let x = line.x;
-        if (line.following) {
-            const rect = this._painter.measureLastText();
-            x = rect.x + rect.width;
-        }
+    private _renderText(line: IPositionedTextLine, style: Style): void {
+        const x = this._getX(line);
         if (style.shadingColor !== "000000") {
             this._painter.paintLine(x, line.y, x + line.width, line.y, style.shadingColor, style.lineSpacing);
         }
         this._painter.paintText(x, line.y, line.width, line.fitWidth, line.text, style.color, style.justification, style.fontFamily, style.fontSize, style.bold, style.italic);
         if (style.underlineMode !== UnderlineMode.none || style.strike || style.doubleStrike) {
             // Render underline after adding text to DOM.
-            const lastTextRect = this._painter.measureLastText();
-            this.renderUnderline(style, lastTextRect);
+            this._renderUnderline(style);
         }
     }
     
-    private renderUnderline(style: Style, textRect: IRectangle): void {
+    private _renderUnderline(style: Style): void {
         // TODO: Support all underline modes
+        const textRect = this._painter.measureLastText();
         const fontSize = style.fontSize;
         const y = textRect.y + fontSize;
         switch(style.underlineMode) {
@@ -59,5 +55,14 @@ export class TextRenderer {
             this._painter.paintLine(textRect.x, y, textRect.x + textRect.width, y - (fontSize / 2) - 1, style.color, 1);
             this._painter.paintLine(textRect.x, y, textRect.x + textRect.width, y - (fontSize / 2) + 2, style.color, 1);
         }
+    }
+
+    private _getX(line: IPositionedTextLine): number {
+        let x = line.x;
+        if (line.following) {
+            const rect = this._painter.measureLastText();
+            x = rect.x + rect.width;
+        }
+        return x;
     }
 }
