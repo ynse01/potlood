4d42ae16fb1eac24a6ec3035d72f4277b3acac14
ynse <laptop@ynse.nl>
2019-10-14 21:09:40 +0200

    Parse Chart axis and Style

--- src/chart/chart-reader.ts
+++ src/chart/chart-reader.ts
@@ -3,8 +3,9 @@ import { ChartSpace } from "./chart-space.js";
 import { BarChart } from "./bar-chart.js";
 import { ChartSeries } from "./chart-series.js";
 import { ChartValue } from "./chart-value.js";
-import { ChartAxis, ChartAxisPosition, ChartAxisTickMode } from "./chart-axis.js";
+import { ChartAxis, ChartAxisPosition, ChartAxisTickMode, ChartAxisLabelAlignment, ChartAxisCrossMode } from "./chart-axis.js";
 import { ChartSpaceStyle } from "./chart-space-style.js";
+import { Metrics } from "../utils/metrics.js";
 
 export class ChartReader {
     public static readChartFromNode(chartSpaceNode: Node, space: ChartSpace): ChartSpace {
@@ -43,16 +44,79 @@ export class ChartReader {
         return chart;
     }
 
-    private static _readSpaceStyle(_spaceStyleNode: Node): ChartSpaceStyle {
-        return new ChartSpaceStyle();
+    private static _readSpaceStyle(spaceStyleNode: Node): ChartSpaceStyle {
+        const style = new ChartSpaceStyle();
+        spaceStyleNode.childNodes.forEach(child => {
+            switch (child.nodeName) {
+                case "a:noFill":
+                    style.fillColor = undefined;
+                    break;
+                case "a:solidFill":
+                    style.fillColor = this._readFillColor(child);
+                    break;
+                case "a:ln":
+                    const firstChild = child.firstChild;
+                    if (firstChild !== null) {
+                        style.lineColor = this._readFillColor(firstChild);
+                    }
+                    break;
+            }
+        });
+        return style;
     }
 
-    private static _readChartAxis(_axisNode: Node): ChartAxis {
+    private static _readChartAxis(axisNode: Node): ChartAxis {
         let pos: ChartAxisPosition = ChartAxisPosition.Bottom;
         let majorTickMode: ChartAxisTickMode = ChartAxisTickMode.None;
         let minorTickMode: ChartAxisTickMode = ChartAxisTickMode.None;
+        let labelAlignment: ChartAxisLabelAlignment = ChartAxisLabelAlignment.Center;
+        let crossMode: ChartAxisCrossMode = ChartAxisCrossMode.AutoZero;
         let labelOffset = 0;
-        return new ChartAxis(pos, majorTickMode, minorTickMode, labelOffset);
+        axisNode.childNodes.forEach(child => {
+            let valAttr: string | undefined = undefined;
+            switch (child.nodeName) {
+                case "c:majorTickMark":
+                    valAttr = Xml.getAttribute(child, "val");
+                    if (valAttr !== undefined) {
+                        majorTickMode = this._parseTickMode(valAttr);
+                    }
+                    break;
+                case "c:minorTickMark":
+                    valAttr = Xml.getAttribute(child, "val");
+                    if (valAttr !== undefined) {
+                        minorTickMode = this._parseTickMode(valAttr);
+                    }
+                    break;
+                case "c:axPos":
+                    valAttr = Xml.getAttribute(child, "val");
+                    if (valAttr !== undefined) {
+                        pos = this._parsePosition(valAttr);
+                    }
+                    break;
+                case "c:lblAlgn":
+                    valAttr = Xml.getAttribute(child, "val");
+                    if (valAttr !== undefined) {
+                        labelAlignment = this._parseLabelAlignment(valAttr);
+                    }
+                    break;
+                case "c:lblOffset":
+                    valAttr = Xml.getAttribute(child, "val");
+                    if (valAttr !== undefined) {
+                        labelOffset = Metrics.convertTwipsToPixels(parseInt(valAttr, 10));
+                    }
+                    break;
+                case "c:crosses":
+                    valAttr = Xml.getAttribute(child, "val");
+                    if (valAttr !== undefined) {
+                        crossMode = this._parseCrossMode(valAttr);
+                    }
+                    break;
+            }
+        });
+        const axis = new ChartAxis(pos, majorTickMode, minorTickMode, labelOffset);
+        axis.labelAlignment = labelAlignment;
+        axis.crossMode = crossMode;
+        return axis;
     }
 
     private static _readChartSeries(seriesNode: Node): ChartSeries {
@@ -152,6 +216,52 @@ export class ChartReader {
             });
         }
         return ref;
-        
+    }
+
+    private static _readFillColor(fillNode: Node): string {
+        let color = "ffffff";
+        const colorNode = fillNode.firstChild;
+        if (colorNode !== null && colorNode.nodeName === "a:srgbClr") {
+            const valAttr = Xml.getAttribute(colorNode, "val");
+            if (valAttr !== undefined) {
+                color = valAttr;
+            }
+        }
+        return color;
+    }
+
+    private static _parseTickMode(tickAttr: string): ChartAxisTickMode {
+        let tickMode = ChartAxisTickMode.None;
+        if (tickAttr === "out") {
+            tickMode = ChartAxisTickMode.Outwards
+        }
+        return tickMode;
+    }
+
+    private static _parsePosition(posAttr: string): ChartAxisPosition {
+        let posMode = ChartAxisPosition.Bottom;
+        switch(posAttr) {
+            case "t":
+                posMode = ChartAxisPosition.Top;
+                break;
+            case "b":
+                posMode = ChartAxisPosition.Bottom;
+                break;
+            case "l":
+                posMode = ChartAxisPosition.Left;
+                break;
+            case "r":
+                posMode = ChartAxisPosition.Right;
+                break;
+        }
+        return posMode;
+    }
+
+    private static _parseLabelAlignment(_alignAttr: string): ChartAxisLabelAlignment {
+        return ChartAxisLabelAlignment.Center;
+    }
+
+    private static _parseCrossMode(_crossAttr: string): ChartAxisCrossMode {
+        return ChartAxisCrossMode.AutoZero;
     }
 }
