189ceee2f8cb84fefecfab52f2780cb794986a53
ynse <laptop@ynse.nl>
2019-12-21 21:40:27 +0100

    Add some first preset shapes

--- src/drawing/preset-shape-factory.ts
+++ src/drawing/preset-shape-factory.ts
@@ -1,8 +1,16 @@
 import { Shape } from "./shape.js";
+import { Vector } from "../math/vector.js";
 
 export class PresetShapeFactory {
     private static creators: any = {
-        "ellipse": PresetShapeFactory._createEllipse
+        "line": PresetShapeFactory._createLine,
+        "ellipse": PresetShapeFactory._createEllipse,
+        "blockArc": PresetShapeFactory._createBlockArc,
+        "smileyFace": PresetShapeFactory._createSmileyFace,
+        "wedgeRectCallout": PresetShapeFactory._createWedgeRectangularCallout,
+        "flowChartCollate": PresetShapeFactory._createFlowChartCollate,
+        "flowChartPunchedCard": PresetShapeFactory._createFlowChartPunchedCard,
+        "flowChartSort": PresetShapeFactory._createFlowChartSort
     }
 
     public createShape(name: string): Shape | undefined {
@@ -14,7 +22,39 @@ export class PresetShapeFactory {
         return shape;
     }
 
+    private static _createBlockArc(): Shape {
+        return new Shape();
+    }
+
     private static _createEllipse(): Shape {
         return new Shape();
     }
+
+    private static _createFlowChartCollate(): Shape {
+        return new Shape();
+    }
+
+    private static _createFlowChartPunchedCard(): Shape {
+        return new Shape();
+    }
+
+    private static _createFlowChartSort(): Shape {
+        return new Shape();
+    }
+
+    private static _createLine(): Shape {
+        const line = new Shape();
+        line.addMove(new Vector(0, 0));
+        line.addLine(new Vector(1, 1));
+        return line;
+    }
+
+    private static _createSmileyFace(): Shape {
+        return new Shape();
+    }
+
+    private static _createWedgeRectangularCallout(): Shape {
+        return new Shape();
+    }
+
 }
