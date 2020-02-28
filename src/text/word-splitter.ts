0506cefd07511c5ca3a0a0a73e5f8edb6a5e80c2
ynse <laptop@ynse.nl>
2020-02-28 10:06:24 +0100

    Expose seperators for word boundaries

--- src/text/word-splitter.ts
+++ src/text/word-splitter.ts
@@ -2,12 +2,14 @@
 export enum WordSeperator {
     Space,
     Dash,
-    Tab
+    Tab,
+    LineFeed
 }
 
 export class WordSplitter {
     private _txt: string;
     private _words: string[] | undefined;
+    private _seperators: WordSeperator[] | undefined;
 
     constructor(txt: string){
         this._txt = txt;
@@ -20,7 +22,41 @@ export class WordSplitter {
         return this._words || [];
     }
 
+    public getSeperator(index: number): WordSeperator {
+        if (this._words === undefined) {
+            this._split();
+        }
+        return this._seperators![index];
+    }
+
     private _split(): void {
-        this._words = this._txt.split(/[\s-]/);
+        this._words = this._txt.split(/[\s-\t]/);
+        const seperators: WordSeperator[] = [];
+        let index = 0;
+        for (let i = 1; i < this._words.length; i++) {
+            index += this._words[i - 1].length;
+            const currentChar = this._txt.charAt(index);
+            switch(currentChar) {
+                case " ":
+                    seperators.push(WordSeperator.Space);
+                    break;
+                case "-":
+                    seperators.push(WordSeperator.Dash);
+                    break;
+                case "\t":
+                    seperators.push(WordSeperator.Tab);
+                    break;
+                case "\r":
+                case "\n":
+                    seperators.push(WordSeperator.LineFeed);
+                    break;
+                default:
+                    console.log("Invalid seperator character found, assuming a space.");
+                    seperators.push(WordSeperator.Space);
+                    break;
+            }
+            index++;
+        }
+        this._seperators = seperators;
     }
 }
