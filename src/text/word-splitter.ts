4d661c7fb0d336fce0cbf7c5461c1b2a4f1bf861
ynse <laptop@ynse.nl>
2020-02-28 09:42:46 +0100

    Split words lazilly

--- src/text/word-splitter.ts
+++ src/text/word-splitter.ts
@@ -6,9 +6,21 @@ export enum WordSeperator {
 }
 
 export class WordSplitter {
-    public words: string[];
-    
+    private _txt: string;
+    private _words: string[] | undefined;
+
     constructor(txt: string){
-        this.words = txt.split(/[\s-]/);
+        this._txt = txt;
+    }
+
+    public get words(): string[] {
+        if (this._words === undefined) {
+            this._split();
+        }
+        return this._words || [];
+    }
+
+    private _split(): void {
+        this._words = this._txt.split(/[\s-]/);
     }
 }
