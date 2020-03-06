85ea68504997db2dcbeacdcd61beb6490fc64ce6
ynse <laptop@ynse.nl>
2020-03-06 21:49:12 +0100

    Read matrix column justification

--- src/math/matrix-column-style.ts
+++ src/math/matrix-column-style.ts
@@ -2,16 +2,25 @@ import { MatrixColumnXAlign } from "./matrix-style.js";
 
 export class MatrixColumnStyle {
     public count: number = 0;
-    public justification: MatrixColumnXAlign = MatrixColumnXAlign.Left;
+    public justification: MatrixColumnXAlign = MatrixColumnXAlign.Center;
 
     public setJustification(jcStr: string | undefined): void {
         switch (jcStr) {
-            case "center":
-            case "right":
-            case "inside":
-            case "outside":
             case "left":
+                this.justification = MatrixColumnXAlign.Left;
+                break;
+            case "right":
+                this.justification = MatrixColumnXAlign.Right;
+                break;
+            case "inside":
+                this.justification = MatrixColumnXAlign.Inside;
+                break;
+            case "outside":
+                this.justification = MatrixColumnXAlign.Outside;
+                break;
+            case "center":
             default:
+                this.justification = MatrixColumnXAlign.Center;
                 break;
         }
     }
