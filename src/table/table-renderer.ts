88539b9810af0f82d05d289e07cd7ae60e85e5a7
ynse <laptop@ynse.nl>
2020-03-22 16:08:22 +0100

    Refactor table border line rendering

--- src/table/table-renderer.ts
+++ src/table/table-renderer.ts
@@ -6,6 +6,19 @@ import { ParagraphRenderer } from "../paragraph/paragraph-renderer.js";
 import { TableBorderSet } from "./table-border-set.js";
 import { TableBorderType } from "./table-border.js";
 
+interface IColoredLine {
+    x1: number;
+    y1: number;
+    x2: number;
+    y2: number;
+    color: string;
+}
+
+interface ISubLine {
+    pos: number;
+    width: number;
+}
+
 export class TableRenderer {
     private _parRenderer: ParagraphRenderer;
     private _painter: IPainter;
@@ -60,44 +73,52 @@ export class TableRenderer {
             if (outerBorders.borderTop !== undefined) {
                 this._renderBorderPart(
                     outerBorders.borderTop.type,
-                    bounds.left,
-                    bounds.top,
-                    bounds.right,
-                    bounds.top,
-                    outerBorders.borderTop.color,
+                    {
+                        x1: bounds.left,
+                        y1: bounds.top,
+                        x2: bounds.right,
+                        y2: bounds.top,
+                        color: outerBorders.borderTop.color
+                    },
                     outerBorders.borderTop.size
                 );
             }
             if (outerBorders.borderBottom !== undefined) {
                 this._renderBorderPart(
                     outerBorders.borderBottom.type,
-                    bounds.left,
-                    bounds.bottom,
-                    bounds.right,
-                    bounds.bottom,
-                    outerBorders.borderBottom.color,
+                    {
+                        x1: bounds.right,
+                        y1: bounds.bottom,
+                        x2: bounds.left,
+                        y2: bounds.bottom,
+                        color: outerBorders.borderBottom.color
+                    },
                     outerBorders.borderBottom.size
                 );
             }
             if (outerBorders.borderStart !== undefined) {
                 this._renderBorderPart(
                     outerBorders.borderStart.type,
-                    bounds.x,
-                    bounds.top,
-                    bounds.x,
-                    bounds.bottom,
-                    outerBorders.borderStart.color,
+                    {
+                        x1: bounds.x,
+                        y1: bounds.bottom,
+                        x2: bounds.x,
+                        y2: bounds.top,
+                        color: outerBorders.borderStart.color
+                    },
                     outerBorders.borderStart.size
                 );
             }
             if (outerBorders.borderEnd !== undefined) {
                 this._renderBorderPart(
                     outerBorders.borderEnd.type,
-                    bounds.right,
-                    bounds.top,
-                    bounds.right,
-                    bounds.bottom,
-                    outerBorders.borderEnd.color,
+                    {
+                        x1: bounds.right,
+                        y1: bounds.top,
+                        x2: bounds.right,
+                        y2: bounds.bottom,
+                        color: outerBorders.borderEnd.color
+                    },
                     outerBorders.borderEnd.size
                 );
             }
@@ -107,66 +128,95 @@ export class TableRenderer {
             if (innerBorders.borderTop !== undefined) {
                 this._renderBorderPart(
                     innerBorders.borderTop.type,
-                    bounds.left,
-                    bounds.top,
-                    bounds.right,
-                    bounds.top,
-                    innerBorders.borderTop.color,
+                    {
+                        x1: bounds.left,
+                        y1: bounds.top,
+                        x2: bounds.right,
+                        y2: bounds.top,
+                        color: innerBorders.borderTop.color
+                    },
                     innerBorders.borderTop.size
                 );
             }
             if (innerBorders.borderBottom !== undefined) {
                 this._renderBorderPart(
                     innerBorders.borderBottom.type,
-                    bounds.left,
-                    bounds.bottom,
-                    bounds.right,
-                    bounds.bottom,
-                    innerBorders.borderBottom.color,
+                    {
+                        x1: bounds.right,
+                        y1: bounds.bottom,
+                        x2: bounds.left,
+                        y2: bounds.bottom,
+                        color: innerBorders.borderBottom.color
+                    },
                     innerBorders.borderBottom.size
                 );
             }
             if (innerBorders.borderStart !== undefined) {
                 this._renderBorderPart(
                     innerBorders.borderStart.type,
-                    bounds.x,
-                    bounds.top,
-                    bounds.x,
-                    bounds.bottom,
-                    innerBorders.borderStart.color,
+                    {
+                        x1: bounds.x,
+                        y1: bounds.bottom,
+                        x2: bounds.x,
+                        y2: bounds.top,
+                        color: innerBorders.borderStart.color
+                    },
                     innerBorders.borderStart.size
                 );
             }
             if (innerBorders.borderEnd !== undefined) {
                 this._renderBorderPart(
                     innerBorders.borderEnd.type,
-                    bounds.right,
-                    bounds.top,
-                    bounds.right,
-                    bounds.bottom,
-                    innerBorders.borderEnd.color,
+                    {
+                        x1: bounds.right,
+                        y1: bounds.top,
+                        x2: bounds.right,
+                        y2: bounds.bottom,
+                        color: innerBorders.borderEnd.color
+                    },
                     innerBorders.borderEnd.size
                 );
             }
         }
     }
     
-    private _renderBorderPart(
-        borderType: TableBorderType,
-        x1: number,
-        y1: number,
-        x2: number,
-        y2: number,
-        color: string,
-        size: number
-    ): void {
+    private _renderBorderPart(borderType: TableBorderType, line: IColoredLine, size: number): void {
+        let relativeSize = size;
         switch (borderType) {
             case TableBorderType.None:
                 break;
+            case TableBorderType.Double:
+                this._renderBorderSubLines(line, [{ pos: 0, width: size * 2}]);
+                break;
+            case TableBorderType.Triple:
+                this._renderBorderSubLines(line, [{ pos: 0, width: size * 3}]);
+                break;
+            case TableBorderType.ThickThinLargeGap:
+                relativeSize /= 12;
+                this._renderBorderSubLines(line, [
+                    { pos: 2.5 * relativeSize, width: 5 * relativeSize},
+                    { pos: 10.5 * relativeSize, width: relativeSize}
+                ]);
+                break;
             case TableBorderType.Single:
             default:
-                this._painter.paintLine(x1, y1, x2, y2, color, size);
+                this._renderBorderSubLines(line, [{pos: 0, width: size}]);
                 break;
         }
+    }
+
+    private _renderBorderSubLines(line: IColoredLine, subLines: ISubLine[]): void {
+        const xDirection = (line.x1 === line.x2) ? ((line.y1 < line.y2) ? -1 : 1) : 0;
+        const yDirection = (line.y1 === line.y2) ? ((line.x1 > line.x2) ? -1 : 1) : 0;
+        subLines.forEach(sub => {
+            this._painter.paintLine(
+                line.x1 + xDirection * sub.pos,
+                line.y1 + yDirection * sub.pos,
+                line.x2 + xDirection * sub.pos,
+                line.y2 + yDirection * sub.pos,
+                line.color,
+                sub.width
+            );
+        });
     }
 }
